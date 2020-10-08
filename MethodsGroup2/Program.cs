58b761fec85732c5c0feb3629fe2b731e1f8adbf
Nico De Witte <nico.dewitte@vives.be>
2020-10-08 16:47:37 +0200

    Class demo code

---
+++ MethodsGroup2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MethodsGroup2
+{
+    class Program
+    {
+        // Lets send a greeting message to the terminal
+        // No return value (output), no arguments (inputs)
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to this group 2 app.");
+        }
+
+        //Request the user's name and return the result as a string
+        public static string RequestUserName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;        // Give back the users name
+        }
+        
+        // Greet a user (name is provided via argument)
+        public static void GreetUser(string user)
+        {
+            Console.WriteLine("Hello " + user);
+        }
+
+        // A method that throws a die between min and max
+        public static int ThrowDie(int minimum, int maximum)
+        {
+            Random generator = new Random();
+            int number = generator.Next(minimum, maximum+1);
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+
+            string username = RequestUserName();
+
+            GreetUser(username);
+
+            int die = ThrowDie(1, 6);
+            Console.WriteLine(die);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - Methods
+
+Demo code of chapter 7 - Methods.
+
+## How to get this locally:
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/MethodsGroup2.git
+```
